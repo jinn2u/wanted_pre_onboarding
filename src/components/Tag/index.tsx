ba9f090fe38d7e1dc91923bb52f4362e1714d653
jinn2u <bsybear623@gmail.com>
2022-02-02 20:06:53 +0900

    feat: 상위 컴포넌트에서 wordList를 알 수 있도록 변경

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,33 +1,31 @@
 import { Input, Wrapper } from './style';
-import React, { KeyboardEvent, useCallback, useState } from 'react';
+import React, { Dispatch, KeyboardEvent, SetStateAction, useCallback, useState } from 'react';
 import WordBox from './WrodBox';
 
 interface Props {
   width?: number | string;
-
-  fontSize?: number;
+  setWordList: Dispatch<SetStateAction<string[]>>;
+  wordList: string[];
 }
-const Tag = ({ width = 400 }: Props) => {
-  const [words, setWords] = useState<string[]>([]);
-
+const Tag = ({ width = 400, setWordList, wordList }: Props) => {
   const handleKeyUp = useCallback((e: KeyboardEvent) => {
     const { value } = e.target as HTMLInputElement;
     if (!value.length || e.key !== 'Enter') {
       return;
     }
-    setWords((prevWords) => [...prevWords, value]);
+    setWordList((prevWords) => [...prevWords, value]);
     (e.target as HTMLInputElement).value = '';
   }, []);
 
   const onErase = useCallback((idx: number) => {
-    setWords((prevWords) => prevWords.filter((_, index) => idx !== index));
+    setWordList((prevWords) => prevWords.filter((_, index) => idx !== index));
   }, []);
 
   return (
     <>
       <Wrapper width={width}>
         <ul>
-          {words.map((word, idx) => (
+          {wordList.map((word, idx) => (
             <li key={idx}>
               <WordBox word={word} onErase={() => onErase(idx)} />
             </li>
