ba95fcc6c506a50e1015033a087c3185af6527fc
jinn2u <bsybear623@gmail.com>
2022-02-03 23:10:06 +0900

    fix: 중복함수 util함수로 빼기

--- src/components/AutoComplete/utils.ts
+++ src/components/AutoComplete/utils.ts
@@ -1,7 +1,20 @@
-export const createMatchedData = (
-  wordList: string[],
-  includeWord: string,
-): { word: string; isSelected: boolean }[] =>
+import { Dispatch, SetStateAction } from 'react';
+import { matchedType } from '.';
+
+export const createMatchedData = (wordList: string[], includeWord: string): matchedType =>
   wordList
     .filter((cash) => cash.includes(includeWord))
     .map((word) => ({ word, isSelected: false }));
+
+export const useUpdateInputAndCloseMated = (
+  word: string,
+  relatedWord: string[],
+  setShowMatched: Dispatch<SetStateAction<boolean>>,
+  setAutoCompleteInput: Dispatch<SetStateAction<string>>,
+  setMatched: Dispatch<SetStateAction<matchedType>>,
+) => {
+  setShowMatched(false);
+  setAutoCompleteInput(word);
+  const filteredCashes = createMatchedData(relatedWord, word);
+  setMatched(filteredCashes);
+};
