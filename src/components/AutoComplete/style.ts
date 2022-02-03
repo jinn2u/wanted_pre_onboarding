acb3b2fe3643cf4c4a8ca9b1c35390c66feec374
jinn2u <bsybear623@gmail.com>
2022-02-03 19:58:28 +0900

    style: autoComplete컴포넌트 스타일링

---
+++ src/components/AutoComplete/style.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const Wrapper = styled.div`
+  position: relative;
+  width: fit-content;
+`;
+export const Input = styled.input<{ width: number }>`
+  width: ${({ width }) => `${width}px`};
+  height: 30px;
+  outline: none;
+  border-radius: 10px;
+  border: 1px solid lightgray;
+  box-sizing: border-box;
+`;
+export const Ul = styled.ul<{ width: number }>`
+  width: ${({ width }) => `${width}px`};
+  border-radius: 10px;
+  top: 30px;
+  box-sizing: border-box;
+  padding: 0px;
+  margin: 0px;
+  list-style: none;
+  /* background-color: orange; */
+  position: absolute;
+  z-index: 100;
+`;
+
+export const Li = styled.li<{ isSelected: boolean }>`
+  background-color: ${({ isSelected }) => (isSelected ? 'lightgray' : 'white')};
+`;
