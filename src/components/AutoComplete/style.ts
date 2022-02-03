c7d37bd7c998f05a45de98a9e404ecf538ab9f38
jinn2u <bsybear623@gmail.com>
2022-02-03 21:54:26 +0900

    style: AutoComplete 스타일링

--- src/components/AutoComplete/style.ts
+++ src/components/AutoComplete/style.ts
@@ -6,18 +6,18 @@ export const Wrapper = styled.div`
 `;
 export const Input = styled.input<{ width: number }>`
   width: ${({ width }) => `${width}px`};
+  padding: 5px 0px 5px 5px;
   height: 30px;
   outline: none;
   border-radius: 10px;
   border: 1px solid lightgray;
-  box-sizing: border-box;
 `;
 export const Ul = styled.ul<{ width: number }>`
-  width: ${({ width }) => `${width}px`};
-  border-radius: 10px;
+  width: ${({ width }) => `${width + 5}px`};
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
   border: 1px solid lightgray;
   top: 30px;
-  box-sizing: border-box;
   padding: 0px;
   margin: 0px;
   list-style: none;
@@ -26,9 +26,15 @@ export const Ul = styled.ul<{ width: number }>`
 `;
 
 export const Li = styled.li<{ isSelected: boolean }>`
+  padding: 0px 0px 0px 5px;
+  margin: 0px;
   background-color: ${({ isSelected }) => (isSelected ? 'lightgray' : 'white')};
   &:hover {
     background-color: lightgray;
     cursor: pointer;
+  }
+  &:last-of-type {
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
   }
 `;
