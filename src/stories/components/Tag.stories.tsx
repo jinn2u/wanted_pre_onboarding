e64b1b5395299b432a10144857f43dfcd9927047
jinn2u <bsybear623@gmail.com>
2022-02-06 18:44:07 +0900

    feat: Tag Component storybook

---
+++ src/stories/components/Tag.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useState } from 'react';
+import { Tag } from '../../components';
+import { Container } from '../../style';
+
+export default {
+  title: 'Example/Tag',
+  components: Tag,
+  argTypes: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Container>
+          <Story />
+        </Container>
+      );
+    },
+  ],
+} as ComponentMeta<typeof Tag>;
+
+export const Default: ComponentStory<typeof Tag> = (args) => {
+  const [wordList, setWordList] = useState<string[]>([]);
+  return <Tag {...args} setWordList={setWordList} wordList={wordList} />;
+};
+
+export const FullWidth: ComponentStory<typeof Tag> = (args) => {
+  const [wordList, setWordList] = useState<string[]>([]);
+  return <Tag {...args} width="100%" setWordList={setWordList} wordList={wordList} />;
+};
