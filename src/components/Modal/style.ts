db162d5a35a8f0272113b5515b741f7232587db8
jinn2u <bsybear623@gmail.com>
2022-02-08 20:10:38 +0900

    fix: z-index적용

--- src/components/Modal/style.ts
+++ src/components/Modal/style.ts
@@ -5,13 +5,18 @@ export const Dim = styled.div`
   left: 0;
   top: 0;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   background: rgba(0, 0, 0, 0.75);
+  z-index: 10000;
 `;
 export const Wrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   padding: 0 20px 20px 20px;
   background-color: white;
   border-radius: 10px;
@@ -19,6 +24,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  z-index: 100001;
 `;
 export const CloseBtn = styled.button`
   border: none;
