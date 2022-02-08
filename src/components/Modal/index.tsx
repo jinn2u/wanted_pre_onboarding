4a0fa7014dfec05112e4cb18bfd82be3c6c8049f
jinn2u <bsybear623@gmail.com>
2022-02-08 20:34:46 +0900

    fix: 로직을 별도의 훅으로 분리

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,54 +1,20 @@
-import { CloseBtn, Dim, Wrapper } from './style';
 import ReactDOM from 'react-dom';
-import { Dispatch, MutableRefObject, SetStateAction, useCallback, useEffect, useMemo } from 'react';
-import { useClickAway } from './useClickAway';
+import { Dispatch, SetStateAction } from 'react';
+import BaseModal from './BaseModal';
+import { useCreateElement } from './useCreateElement';
 
-interface Props {
+export interface Props {
   children: React.ReactNode;
   setIsModalOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const ModalBase = ({ children, setIsModalOpen, ...props }: Props) => {
-  const handleCloseBtn = useCallback(() => {
-    setIsModalOpen(false);
-  }, []);
-  const ref = useClickAway(handleCloseBtn);
-
-  useEffect(() => {
-    document.body.style.overflow = 'hidden';
-    return () => {
-      document.body.style.overflow = 'auto';
-    };
-  }, []);
-  return (
-    <Dim>
-      <Wrapper className="modal" ref={ref as MutableRefObject<HTMLDivElement>} {...props}>
-        <CloseBtn type="button" onClick={handleCloseBtn}>
-          x
-        </CloseBtn>
-        {children}
-      </Wrapper>
-    </Dim>
-  );
-};
 const Modal = ({ children, setIsModalOpen, ...props }: Props) => {
-  const el = useMemo(() => {
-    const $el = document.createElement('div');
-    $el.className = 'portal-modal';
-    return $el;
-  }, []);
-
-  useEffect(() => {
-    document.body.appendChild(el);
-    return () => {
-      document.body.removeChild(el);
-    };
-  });
+  const el = useCreateElement('portal-modal');
 
   return ReactDOM.createPortal(
-    <ModalBase {...props} setIsModalOpen={setIsModalOpen}>
+    <BaseModal {...props} setIsModalOpen={setIsModalOpen}>
       {children}
-    </ModalBase>,
+    </BaseModal>,
     el,
   );
 };
