fef141f9cb7d5fe8ea1c84aa45113807f4842cdf
jinn2u <bsybear623@gmail.com>
2022-02-08 20:35:00 +0900

    fix: BaseModal 컴포넌트 분리

---
+++ src/components/Modal/BaseModal.tsx
@@ -0,0 +1,31 @@
+import { useClickAway } from './useClickAway';
+import { usePreventScroll } from './usePreventScroll';
+import { CloseBtn, Dim, Wrapper } from './style';
+import { MutableRefObject, useCallback } from 'react';
+import { Props } from '.';
+
+const BaseModal = ({ children, setIsModalOpen, ...props }: Props) => {
+  usePreventScroll();
+
+  const handleCloseBtn = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+  const ref = useClickAway(handleCloseBtn);
+
+  return (
+    <Dim>
+      <Wrapper
+        tabIndex={0}
+        className="modal"
+        ref={ref as MutableRefObject<HTMLDivElement>}
+        {...props}
+      >
+        <CloseBtn type="button" onClick={handleCloseBtn}>
+          x
+        </CloseBtn>
+        {children}
+      </Wrapper>
+    </Dim>
+  );
+};
+export default BaseModal;
